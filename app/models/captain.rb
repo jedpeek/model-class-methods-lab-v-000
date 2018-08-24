8c1ca8e1d26c6e3e539ebb26426adde3ba69b577
Jed Peek <jedpeek@yahoo.com>
2018-08-24 18:10:42 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -10,6 +10,6 @@ class Captain < ActiveRecord::Base
   end
 
   def self.talented_seafarers
-    includes(boats: :classifications).where('classifications.name = ? or ?', 'Sailboat, Motorboat').uniq
+    includes(boats: :classifications).where('classifications.name = ? or ?', 'Sailboat' 'Motorboat').uniq
   end
 end
