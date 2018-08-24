e0733f605d56844775757aa8534fe1ab04789663
Jed Peek <jedpeek@yahoo.com>
2018-08-24 17:59:14 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,7 +28,7 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    Boat.where('boat.classifications_ids >= ?', 3)
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.longest
